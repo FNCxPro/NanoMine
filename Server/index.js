26835ee4b050a10d081b9ce34e8c2f2ac4ca02b1
Seth Stephens <relative@airmail.cc>
2017-12-10 06:59:49 -0500

    debug

--- Server/index.js
+++ Server/index.js
@@ -54,6 +54,7 @@ wss.on('connection', (ws) => {
       case 'MINE_START':
         if(state.mining || state.miner) break
         const {algorithm, pool, username, password} = event.payload
+        console.log(['-a', `${algorithm}`, '-o', `"${pool}"`, '-u', `"${username}"`, '-p', `"${password}"`].join(' '))
         state.miner = spawn('ccminer-x64', ['-a', `${algorithm}`, '-o', `"${pool}"`, '-u', `"${username}"`, '-p', `"${password}"`], {
           cwd: path.join(__dirname, '..', 'Binaries', 'ccminer'),
           windowsHide: true
